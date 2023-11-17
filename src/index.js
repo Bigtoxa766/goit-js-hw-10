8ea5386c809a66ffa566caa7382245cf924934ef
Bigtoxa766 <bigtoxa766@gmail.com>
2023-11-17 19:27:01 +0200

    render+info

--- src/index.js
+++ src/index.js
@@ -6,15 +6,38 @@ const loaderEl = document.querySelector('.loader');
 const errorEl = document.querySelector('.error');
 const infoEl = document.querySelector('.cat-info');
 
+loaderEl.style.display = 'none';
+errorEl.style.display = 'none';
+
 selectorEl.addEventListener('change', onSearch);
 
 function onSearch(e) {
   const elementId = e.target.value;
-  fetchCatByBreed(elementId).then(item => console.log(item))
+
+  fetchCatByBreed(elementId).then(data =>
+    data.map(item => {
+      infoEl.innerHTML = `<img src="${item.url}" alt="" width="500px">`;
+      
+      fetchBreeds().then(data => {
+        data.map(({name, description, temperament, id}) => {
+      if (id === elementId) {
+        
+        const markup = `
+        <div class="wrapper-js">
+        <h1 class="name-js">${name}</h1>
+      <p class="description-js">${description}</p>
+      <p><b>Temperament:</b> ${temperament}</p>
+      </div>`;
+        
+       return infoEl.insertAdjacentHTML('beforeend', markup)
+      }
+    })
+  })
+    }));
+ 
 }
 
 fetchBreeds().then(data => {
-  console.log(data);
 
   data.map(({ id, name }) => {
     const markup = `
