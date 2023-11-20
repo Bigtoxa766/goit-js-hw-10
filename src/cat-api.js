2cf3ffd93ff934cc84d888f8139762ef9ace65ff
Bigtoxa766 <bigtoxa766@gmail.com>
2023-11-20 12:27:19 +0200

    pre-final

--- src/cat-api.js
+++ src/cat-api.js
@@ -2,13 +2,17 @@ import axios from "axios";
 // 'live_qghZgDxUadKGF3wnRtHDGKXMRWjfgkLv8WMQTRSKnZsBSBaENJvhkvripMcVMkOf';
 
 axios.defaults.headers.common["x-api-key"] = "live_qghZgDxUadKGF3wnRtHDGKXMRWjfgkLv8WMQTRSKnZsBSBaENJvhkvripMcVMkOf";
+import { loaderEl } from './index';
 
 export function fetchBreeds() {
+  loaderEl.style.display = 'inline-block';
+
   return fetch('https://api.thecatapi.com/v1/breeds')
     .then(resp => {
-    if (!resp.ok) {
-      throw new Error (resp.statusText)
+      if (!resp.ok) {
+      throw new Error(resp.statusText)
       }
+      loaderEl.style.display = 'none';
       return resp.json()
   })
 };
--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
+import SlimSelect from 'slim-select';
+import Notiflix from 'notiflix';
+
 import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
 
 const selectorEl = document.querySelector('.breed-select');
 const loaderEl = document.querySelector('.loader');
-const errorEl = document.querySelector('.error');
+// const errorEl = document.querySelector('.error');
 const infoEl = document.querySelector('.cat-info');
 
-loaderEl.style.display = 'none';
-errorEl.style.display = 'none';
+export { loaderEl };
 
+  
 selectorEl.addEventListener('change', onSearch);
 
 function onSearch(e) {
   const elementId = e.target.value;
 
-  fetchCatByBreed(elementId).then(data =>
+  fetchCatByBreed(elementId).then(data => {
+
+    // loaderEl.classList.remove("js-unActive");
+
     data.map(item => {
-      infoEl.innerHTML = `<img src="${item.url}" alt="" width="500px">`;
+         infoEl.innerHTML = `<img src="${item.url}" alt="" width="500px">`;
       
       fetchBreeds().then(data => {
-        data.map(({name, description, temperament, id}) => {
+        data.map(({ name, description, temperament, id }) => {
+          
       if (id === elementId) {
-        
+        // loaderEl.classList.add("js-unActive");
+
         const markup = `
         <div class="wrapper-js">
         <h1 class="name-js">${name}</h1>
@@ -29,20 +37,36 @@ function onSearch(e) {
       <p><b>Temperament:</b> ${temperament}</p>
       </div>`;
         
-       return infoEl.insertAdjacentHTML('beforeend', markup)
+        return infoEl.insertAdjacentHTML('beforeend', markup)
       }
     })
+      })
+      .catch(err => {
+    if (!err) {
+      return
+          }
+          })
+    })
   })
-    }));
- 
-}
+  .catch(err => {
+    if (!err) {
+      return
+    }
+    Notiflix.Report.failure('Oops! Something went wrong! Try reloading the page!')
+  })
+};
 
 fetchBreeds().then(data => {
-
   data.map(({ id, name }) => {
+    
     const markup = `
     <option value="${id}">${name}</option>`;
     selectorEl.insertAdjacentHTML('beforeend', markup);
-    
   })
-});
+}).catch(err => {
+    if (!err) {
+      return
+    }
+    Notiflix.Report.failure('Oops! Something went wrong! Try reloading the page!')
+  }
+)
